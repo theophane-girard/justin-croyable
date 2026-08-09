416aa94b8101ba12b2787940e4fa03ddf3a6aba6
Théophane <girard.theophane@gmail.com>
2026-08-09 13:52:32 +0200

    add firebase config for front & fix build

--- packages/potager-web/src/app/core/app-config.ts
+++ packages/potager-web/src/app/core/app-config.ts
@@ -1,11 +1,23 @@
-const FIREBASE_PLACEHOLDER_PREFIX = 'A_REMPLACER';
 
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const FIREBASE_PLACEHOLDER_PREFIX = 'A_REMPLACER';
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 export const FIREBASE_CONFIG = {
-  apiKey: 'A_REMPLACER_FIREBASE_API_KEY',
-  authDomain: 'a-remplacer.firebaseapp.com',
-  projectId: 'a-remplacer',
-  appId: 'A_REMPLACER_APP_ID',
-} as const;
+  apiKey: "AIzaSyAJQtpobzZCDWU7B7JmBqI6jvINxjSRp5Q",
+  authDomain: "justin-croyable-story.firebaseapp.com",
+  projectId: "justin-croyable-story",
+  storageBucket: "justin-croyable-story.firebasestorage.app",
+  messagingSenderId: "956067265057",
+  appId: "1:956067265057:web:20a72394a25f8e0a428160",
+  measurementId: "G-TBNFPQVSVV"
+};
+
+// Initialize Firebase
+const app = initializeApp(FIREBASE_CONFIG);
+const analytics = getAnalytics(app);
 
 export const API_BASE_URL = 'http://localhost:3000';
 
